1f5672ca1cd769b27c989d64ed5a4b077c876b7c
snapecode <smasnape.gmail.com>
2018-09-18 01:41:09 -0400

    //Notebook Component - styles. for the notebook page

--- src/components/Notebook.vue
+++ src/components/Notebook.vue
@@ -30,5 +30,52 @@
 </script>
 
 <style scoped>
+  .notebook {
+    max-width: 20rem;
+    width: 30rem;
+    background: #ecfcff;
+  }
 
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    position: relative;
+  }
+
+  li {
+    padding: 1rem;
+    font-size: 1.25rem;
+    min-height: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  li:hover {
+    cursor: pointer;
+    background-color: #a9a9a9;
+  }
+
+  .active {
+    background-color: #1e90ff;
+  }
+
+  .active:hover {
+    background-color: #1e90fd;
+  }
+
+  .new-note {
+    background-color: #4169e1;
+    color: #808080;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .new-note:hover {
+    background-color: #000080;
+  }
 </style>
