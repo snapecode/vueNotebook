0755f74584eb9473ae1ae55b34101ad7372fbdf5
snapecode <smasnape.gmail.com>
2018-09-18 01:31:08 -0400

    //Notebook Component - initialized template, iterate over each page, one per li. Active page if in use, and a click listener. Different li for adding new page.

---
+++ src/components/Notebook.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="notebook">
+  <ul>
+    <li
+        v-for="(note, index) of notes"
+        class="note"
+        v-bind:class="{ 'active': index === activeNote}"
+        @click="changeNote(index)"
+        >
+      <div> {{note.title}}</div>
+    </li>
+    <li class="new-note" @click="newNote()">New Note +</li>
+  </ul>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "notebook"
+    }
+</script>
+
+<style scoped>
+
+</style>
