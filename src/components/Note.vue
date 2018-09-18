b6e7442c50155e3a48f2e7b64d3cf1d061ac3d8f
snapecode <smasnape.gmail.com>
2018-09-18 01:06:46 -0400

    //Note Component - Initial template for each Note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,11 +1,5 @@
 
-//First we check if we have valid note data to work with and if not, it will display a message
-//telling the user how to start using the app. Once we have a note, we bind two modals, one
-//being note.title and the other being  note.content. Finally, we have our two buttons to handle
-//our two supported operations for this component, deleting the current note, and saving the
-//current note.
-
-<template></template>
+<template>
       <div class="note">
         <div v-if="note">
           <label for="title">Title</label>
